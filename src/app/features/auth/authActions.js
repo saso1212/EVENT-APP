d0a281fb06f05f91bebdfa429cfe8d3fac9c635e
Saso SD. Davidovic <semos@AlkaloidAD.local>
2018-12-29 17:34:21 +0100

    'commit'

--- src/app/features/auth/authActions.js
+++ src/app/features/auth/authActions.js
@@ -1,10 +1,31 @@
-import {LOGIN_USER,SIGN_OUT_USER} from './authConstatnts';
-import {closeModal} from '../modals/modalActions';
-
+import {SubmissionError} from 'redux-form'
+import {SIGN_OUT_USER} from './authConstatnts';
+import {closeModal} from '../modals/modalActions'
+import {toastr} from 'react-redux-toastr'
+// if I want inteleses with firebase I mast use this
+// import firebase from  'firebase'
+//  firebase.auth().signInWithEmailAndPassword
 export const login=(creds)=>{
-    return dispatch =>{
-        dispatch({type:LOGIN_USER, payload:{creds}})
-        dispatch(closeModal())
+    return async (dispatch,getState,{getFirebase}) =>{
+        const firebase=getFirebase();
+        try{
+        await firebase.auth().signInWithEmailAndPassword(creds.email,creds.password);
+         dispatch(closeModal());
+         toastr.success('Succes','You have been succesfuli Login')
+
+        }
+        catch (error){
+            console.log(error);
+           // with this method we well aveable error in the form
+            throw new SubmissionError({
+                _error:error.message
+            })
+            
+            
+            // throw new SubmissionError({
+            //     _error:'Login Failed'
+            // })
+        }
     }
 }
 
